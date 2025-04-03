84766fe6535adc371946ed7c141d52d3a0900d1b
Oshan Maharjan <134091025+oshanma@users.noreply.github.com>
2025-04-02 23:32:58 -0400

    Update gameDaemon.java

--- gameDaemon.java
+++ gameDaemon.java
@@ -1,14 +1,18 @@
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class gameDaemon {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(12345);
-        while (true) {
-            Socket socket = serverSocket.accept();
-            gameThread thread = new gameThread(socket);
-            thread.start();
+    public static void main(String[] args) {
+        int i = 12345; // Define the port number
+        try (ServerSocket serverSocket = new ServerSocket(i)) {
+            System.out.println();
+            System.out.println("CODE: " + i);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new gameThread(socket).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
